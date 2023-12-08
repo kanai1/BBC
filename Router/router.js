447379e412d87c026fac4ceabac2ea7fe101a496
kanai <ksh30918@kakao.com>
2023-12-08 16:15:13 +0900

    FEAT: router.js 테스팅

---
+++ Router/router.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const router = express.Router()
+
+router.use('/login', (req, res, next) => {
+	res.send('login test')
+})
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const router = require('./Router/router')
 
 const app = express()
 
@@ -7,49 +8,8 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}));
 
-app.get('/list', (req, res, next) => {
-	const dumplist1 = [
-		{
-			name: "name1",
-			msg: "msg1"
-		},
-		{
-			name: "name2",
-			msg: "msg2"
-		},
-		{
-			name: "name1",
-			msg: "msg3"
-		},
-		{
-			name: "name1",
-			msg: "msg4"
-		},
-		{
-			name: "name2",
-			msg: "msg5"
-		},
-		{
-			name: "name1",
-			msg: "msg6"
-		},
-	]
-	const dumplist2 = [
-		{
-			name: "name1",
-			msg: "오래된 메시지에요"
-		}
-	]
-	if(req.query.option == 'newest') res.send({code: 200, list: dumplist1, oldestMessageId: 10})
-	else res.send({code: 200, list: dumplist2, oldestMessageId: 20})
+app.use('/api', router)
+
+const server = app.listen(8888, () => {
+	console.log('Server is listening to 8888')
 })
-
-app.post('/send', (req, res, next) => {
-	const name = req.body.name
-	const msg = req.body.message
-
-	console.log(req.body)
-	res.send({code: 200})
-})
-
-const server = app.listen('8888')
