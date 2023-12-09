3499d3844b883291055f072224b72a7deea82b24
qwertyou <chjw1346@gmail.com>
2023-12-09 16:22:29 +0900

    db로 부터 모든 유저의 정보를 가저오는 api 작성

--- Router/router.js
+++ Router/router.js
@@ -1,9 +1,11 @@
-const express = require('express')
-const router = express.Router()
-const APIrouter = require('./api/router')
-const PAGErouter = require('./page/router')
+const express = require("express");
+const router = express.Router();
+const APIrouter = require("./api/router");
+const PAGErouter = require("./page/router");
+const usersRouter = require("./api/users");
 
-router.use('/page', PAGErouter)
-router.use('/api', APIrouter)
+router.use("/page", PAGErouter);
+router.use("/api", APIrouter);
+router.use("/api", usersRouter);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const router = require("./Router/router");
-const dotenv = require("dotenv").config(); // 환경 변수를 불러오기 위해 dotenv를 사용
-const mysql = require("mysql2"); // mysql2 모듈을 불러옴
+const fs = require("fs");
+const path = require("path");
+const db = require("./db");
 
 const app = express();
 
@@ -16,19 +17,20 @@ app.use(express.static(`${__dirname}/public`));
 
 app.use("/", router);
 
-// 데이터베이스 연결 설정
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
-
-db.connect((error) => {
-  if (error) throw error;
-  console.log("Database connected successfully");
-});
-
 const server = app.listen(8888, () => {
   console.log("Server is listening to 8888");
+});
+
+// db 초기 데이터 구성
+const initSql = fs.readFileSync(
+  path.join(__dirname, "lib", "init.sql"),
+  "utf-8"
+);
+
+db.query(initSql, (err, results) => {
+  if (err) {
+    console.error("Error executing init.sql: ", err);
+    process.exit(1);
+  }
+  console.log("init.sql executed successfully");
 });
