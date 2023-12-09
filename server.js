d53d2f3a3b44b708577e8edc65b1535f095630b4
kanai <ksh30918@kakao.com>
2023-12-09 18:07:18 +0900

    DELETE: 필요없는 require삭제

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const router = require("./Router/router");
-const fs = require("fs");
-const path = require("path");
-const db = require("./db");
 
 const app = express();
 
