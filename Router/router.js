aa3e08500ebda7cc7c85798cd9c6a05901497c4b
kanai <ksh30918@kakao.com>
2023-12-09 18:28:53 +0900

    Fix: 라우터에 이상한 코드 지움

--- Router/router.js
+++ Router/router.js
@@ -2,10 +2,8 @@ const express = require("express");
 const router = express.Router();
 const APIrouter = require("./api/router");
 const PAGErouter = require("./page/router");
-const usersRouter = require("./api/users");
 
 router.use("/page", PAGErouter);
 router.use("/api", APIrouter);
-router.use("/api", usersRouter);
 
 module.exports = router;
