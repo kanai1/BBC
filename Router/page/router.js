bdd7d0d1ebeec29b7fc61515c9db81e1c3e7e893
kanai <ksh30918@kakao.com>
2023-12-09 15:23:50 +0900

    Fix: 렌더링 경로오류 수정중 - html파일을 ejs로 변경 - 라우터에서도 이름 변경

--- Router/page/router.js
+++ Router/page/router.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.use('/chat', (req, res, next) => {
-	res.render('chat');
+	res.render('chat.ejs');
 })
 
 module.exports = router;
