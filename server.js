3e0aa27db1c0447c9f7531e06f1263199ddac34d
kanai <ksh30918@kakao.com>
2023-12-10 18:06:15 +0900

    Fix: turn-server.router is not middleware

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const turnServer = createTurnServer();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(turnServer.router)
+app.use(turnServer)
 
 app.set("views", "./public");
 app.set("view engine", "ejs");
