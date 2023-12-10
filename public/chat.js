82b68acb1641de3ba04a73d3779c843d84a51cad
kanai <ksh30918@kakao.com>
2023-12-10 18:03:14 +0900

    FEAT: P2P채팅 구현 테스트

--- public/chat.js
+++ public/chat.js
@@ -1,4 +1,4 @@
-const loadingButton = document.getElementById("LoadingButton")
+/*const loadingButton = document.getElementById("LoadingButton")
 const sendButton = document.getElementById("sendButton")
 
 let webSocket
@@ -80,4 +80,137 @@ function getDataFromSocket(event) {
 	const msg = data.message
 	addMessage(name, msg)
 	console.log(event.data)
+}*/
+const loadingButton = document.getElementById("LoadingButton")
+const sendButton = document.getElementById("sendButton")
+
+let localConnection;
+let dataChannel;
+
+// Offer를 생성하고 상대방에게 전송하는 함수
+async function createOffer() {
+    const configuration = { iceServers: [
+        {
+            urls: 'stun:stun.l.google.com:19302',
+        },
+        { 
+            urls: 'turn:your-turn-server-address:3478', 
+            username: 'testUser', 
+            credential: 'testPass' 
+        }
+    
+    ] }; // ICE 서버는 여기에 추가 가능
+
+    localConnection = new RTCPeerConnection(configuration);
+
+    // 데이터 채널 생성
+    dataChannel = localConnection.createDataChannel('textChannel');
+
+    // 데이터 채널 열릴 때 이벤트 핸들러
+    dataChannel.addEventListener('open', () => {
+        console.log('데이터 채널이 열렸습니다.');
+    });
+
+    // 상대방으로부터 데이터를 받았을 때 이벤트 핸들러
+    dataChannel.addEventListener('message', (event) => {
+        const receivedMessage = event.data;
+        displayMessage(receivedMessage);
+    });
+
+    // Offer 생성
+    const offer = await localConnection.createOffer();
+    await localConnection.setLocalDescription(offer);
+
+    // Offer를 상대방에게 전송
+    sendMessage({ offer });
 }
+
+// Answer를 생성하고 Offer를 수락하는 함수
+async function createAnswer(offer) {
+    const configuration = { iceServers: [] }; // ICE 서버는 여기에 추가 가능
+
+    localConnection = new RTCPeerConnection(configuration);
+
+    // 데이터 채널 생성
+    localConnection.addEventListener('datachannel', (event) => {
+        dataChannel = event.channel;
+
+        // 데이터 채널 열릴 때 이벤트 핸들러
+        dataChannel.addEventListener('open', () => {
+            console.log('데이터 채널이 열렸습니다.');
+        });
+
+        // 상대방으로부터 데이터를 받았을 때 이벤트 핸들러
+        dataChannel.addEventListener('message', (event) => {
+            const receivedMessage = event.data;
+            displayMessage(receivedMessage);
+        });
+    });
+
+    // Offer를 수락하고 Answer 생성
+    await localConnection.setRemoteDescription(new RTCSessionDescription(offer));
+    const answer = await localConnection.createAnswer();
+    await localConnection.setLocalDescription(answer);
+
+    // Answer를 상대방에게 전송
+    sendMessage({ answer });
+}
+
+// ICE candidate를 처리하는 함수
+function handleIceCandidate(event) {
+    if (event.candidate) {
+        sendMessage({ iceCandidate: event.candidate });
+    }
+}
+
+// 메시지를 상대방에게 전송하는 함수
+function sendMessage() {
+    // 상대방에게 메시지 전송 로직을 추가하세요
+    // WebSocket이나 다른 통신 수단을 사용할 수 있습니다.
+
+    // 예시: 데이터 채널을 통해 메시지 전송
+
+	const name = getValue('name', false)
+	const msg = getValue('message')
+
+	const data = {
+		'type': 'msg_send',
+		'name': name,
+		'message': msg
+	}
+
+    if (dataChannel) {
+        dataChannel.send(JSON.stringify(data));
+    }
+}
+
+// Offer를 수락하는 함수
+function acceptOffer(offer) {
+    createAnswer(offer);
+}
+
+// 상대방의 ICE candidate를 처리하는 함수
+function handleRemoteIceCandidate(candidate) {
+    localConnection.addIceCandidate(new RTCIceCandidate(candidate));
+}
+
+// 채팅 로그에 메시지 추가
+function displayMessage(message) {
+    const messageBox = document.getElementById("messageBox")
+	const spanChild = document.createElement('span')
+	const brChild = document.createElement('br')
+
+	spanChild.textContent = `${message.name}: ${message.message}`
+	messageBox.append(spanChild, brChild)
+	messageBox.scrollTop = messageBox.scrollHeight;
+}
+
+function getValue(attribute, init = true) {
+	const attr = document.getElementById(attribute)
+	const value = attr.value
+	attr.value = init?"":attr.value
+	return value
+}
+
+// Offer 생성
+createOffer();
--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 const router = require("./Router/router");
+const { createTurnServer } = require('./turn-server');
 
 const app = express();
+
+const turnServer = createTurnServer();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(turnServer.router)
 
 app.set("views", "./public");
 app.set("view engine", "ejs");
@@ -16,4 +20,12 @@ app.use("/", router);
 
 const server = app.listen(8888, () => {
   console.log("Server is listening to 8888");
+});
+
+process.on('exit', () => {
+  turnServer.stop();
+});
+
+process.on('SIGINT', () => {
+  process.exit();
 });
