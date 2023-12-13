4cd04aa904e0219a7bb25a5ec8a03e7cb007fdad
qwertyou <chjw1346@gmail.com>
2023-12-14 00:38:42 +0900

    여행 api 추가

--- Router/api/router.js
+++ Router/api/router.js
@@ -1,8 +1,10 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const users = require('../../api/users')
+const users = require("../../api/users");
+const travels = require("../../api/travels");
 
-router.use('/users', users.getAllUsers)
+router.use("/users", users.getAllUsers);
+router.use("/travels", travels.getAllTravels);
 
 module.exports = router;
