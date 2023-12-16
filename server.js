ab3d454ef2b2ddf423d4583ddc9d9879014fc8de
kanai <ksh30918@kakao.com>
2023-12-16 14:44:12 +0900

    Fix: 쿠키를 사용할 수 있도록 수정

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const router = require("./Router/router");
+const CookieParser = require('cookie-parser')
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(CookieParser())
 
 app.set("views", "./public");
 app.set("view engine", "ejs");
