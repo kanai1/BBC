af1dbe4880a41bc6a2d8dba76a1e33a7d7c8d819
minseong kim <emforhsqhf29@gmail.com>
2023-12-17 00:40:47 +0900

    message 오류수정

--- public/matches.js
+++ public/matches.js
@@ -1,42 +1,53 @@
 // public/matches.js
-document.getElementById("matchForm").addEventListener("submit", function(e) {
-    e.preventDefault();
+document.addEventListener("DOMContentLoaded", function () {
+    const matchForm = document.getElementById("matchForm");
+    const resultsContainer = document.getElementById("matchResults");
 
-    const destination = document.getElementById("destination").value;
-    const age = document.getElementById("age").value;
-    const gender = document.getElementById("gender").value;
+    matchForm.addEventListener("submit", function (e) {
+        e.preventDefault();
 
-    fetch("/api/matches", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ destination, age, gender })
-    })
-    .then(res => {
-      if (!res.ok) {
-        throw new Error(`서버 응답 오류: ${res.status} ${res.statusText}`);
-      }
-      return res.json();
-    })
-    .then(matches => {
-      const resultsContainer = document.getElementById("matchResults");
-      resultsContainer.innerHTML = "";
+        // 입력 필드에서 값을 가져옵니다.
+        const destination = document.getElementById("destination").value;
+        const age = document.getElementById("age").value;
+        const gender = document.getElementById("gender").value;
 
-      if (matches.length === 0) {
-        resultsContainer.innerHTML = "<p>매칭 결과가 없습니다.</p>";
-        return;
-      }
+        // 서버에 POST 요청을 보냅니다.
+        fetch("/api/matches", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ destination, age, gender })
+        })
+        .then(res => {
+            if (!res.ok) {
+                throw new Error(`서버 응답 오류: ${res.status} ${res.statusText}`);
+            }
+            return res.json();
+        })
+        .then(matches => {
+            resultsContainer.innerHTML = "";
 
-      matches.forEach(match => {
-        const div = document.createElement("div");
-        div.className = "match-entry"; // CSS 클래스를 추가하여 스타일을 적용할 수 있습니다.
-        div.innerHTML = `여행 정보 ID: ${match.travelInfoId}, 사용자 ID: ${match.userId}, 목적지: ${match.destination}, 신뢰도 점수: ${match.trustScore}`;
-        resultsContainer.appendChild(div);
-      });
-    })
-    .catch(error => {
-      console.error('Error:', error);
-      alert(`매칭 요청 중 오류가 발생했습니다: ${error.message}\n오류 위치: ${error.stack}`);
+            if (matches.length === 0) {
+                resultsContainer.innerHTML = "<p>매칭 결과가 없습니다.</p>";
+                return;
+            }
+
+            // 서버로부터 받은 매칭 결과를 화면에 표시합니다.
+            matches.forEach(match => {
+                const div = document.createElement("div");
+                div.className = "match-entry";
+                div.innerHTML = `
+                    여행 정보 ID: ${match.travelInfoId}, 
+                    사용자 ID: ${match.userId}, 
+                    목적지: ${match.destination}, 
+                    신뢰도 점수: ${match.trustScore}`;
+                resultsContainer.appendChild(div);
+            });
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert(`매칭 요청 중 오류가 발생했습니다: ${error.message}`);
+        });
     });
 });
